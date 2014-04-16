8dbdaa3f50e8f43dd41a7d039c4258c03e791aca
jamiewright <jamie@jdhw.co.uk>
2014-04-16 09:52:14 +0100

    tidy up comments for script

--- third_party_cookie_setter.js
+++ third_party_cookie_setter.js
@@ -1,8 +1,43 @@
 /**
+ * Copyright (C) 2014 CrowdSurge
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  *
+ * Fix to set cookies for a third party framed site. If the frame is unable to set a cookie,
+ * the fix is to redirect the user to the third party site in the parent current window, set
+ * a cookie and return to the parent and framed page the user was on.
+ * 
+ * Example:
+ * 
+ * The framed site includes the check if the cookie can be set and whether an attempt has been 
+ * made
+ * 
+ * <script src="/path/to/csThirdPartyCookie.js"></script>
+ * <script>
+ * window.onload= function(){               
+ *     var thirdPartyCookie = new csThirdPartyCookie();
+ *     if (!thirdPartyCookie.check()) {
+ *         // cookies cannot be set so user should be alerted
+ *     }
+ * };
+ * </script>
  *
- * @returns {csThirdPartyCookie.Anonym$0}
+ * The cookie setter sets a cookie and returns to the parent and framed page 
+ * made
+ * 
+ * <script src="/path/to/csThirdPartyCookie.js"></script>
+ * <script>
+ * window.onload= function(){               
+ *     var thirdPartyCookie = new csThirdPartyCookie();
+ *     thirdPartyCookie.setAndReturn();
+ * };
+ * </script>
+ * 
  */
 var csThirdPartyCookie = function () {
     'use strict';
@@ -11,12 +46,19 @@ var csThirdPartyCookie = function () {
      * config options
      */
     var options = {
+        // the cookie to set
         thirdPartyCookieName: 'cs_third_party_accept',
+        // uri parameter appended to parent to check if the fix has already been attempted
         returnUriParameter: 'cookie_attempted',
+        // file that sets a cookie in the parent and returns to the original page
         pathToCookieSetter: '/store/js/default/third_party_cookie_setter.html',
+        // url to return to after cookie set
         returnUriKey: 'return_url',
+        // frame url
         frameUriKey: 'frame_url',
+        // whitelist array of frame uri parts to map to the parent uri
         retainedUriParameters: ['eventid', 'descid'],
+        // the mapping to perform
         retainedUriParameterMapping: {
             eventid: 'event',
             descid: 'desc'
@@ -24,35 +66,61 @@ var csThirdPartyCookie = function () {
     };
 
 
-    // http://stackoverflow.com/questions/11582512/how-to-get-url-parameters-with-javascript
+   
+    /**
+     * Returns the matched uri parameter
+     * 
+     * Source: http://stackoverflow.com/questions/11582512/how-to-get-url-parameters-with-javascript
+     * 
+     * @param {string} name
+     * @param {string} url
+     * @returns {string}
+     */
     function getURLParameter(name, url) {
         return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(url) || ["", ""])[1].replace(/\+/g, '%20')) || null;
     }
 
+    /**
+     * Sets a cookie for the domain
+     */
     function setCookie() {
         var exdate = new Date();
         exdate.setDate(exdate.getDate() + 2);
         document.cookie = options.thirdPartyCookieName + "=1;expires=" + exdate.toUTCString() + "; path=/";
     }
 
+    /**
+     * Check whether the cookie is set
+     * 
+     * @returns {Boolean}
+     */
     function isCookieSet() {
         return (document.cookie.indexOf(options.thirdPartyCookieName) > -1);
     }
 
+    /**
+     * Check if an attempt to set a cookei form the parent window has been attempted
+     * 
+     * @returns {Boolean}
+     */
     function isReturnedFromRedirect() {
         var re = new RegExp("[?|&]" + options.returnUriParameter + "=1", "i");
         return (document.referrer.search(re) > 0);
     }
 
+    /**
+     * Handles cookies cannot be set from frame
+     * 
+     * @returns {Boolean}
+     */
     function unableToSetCookie() {
         console.log('Cookies must be enabled');
         return false;
     }
 
     /**
-     *
-     * redirect to cookie setter in top
-     * pass the top url as a uri parameter
+     * Redirect to cookie setter in parent
+     * passes the top url as a uri parameter
      *
      * console.log(window.location.protocol);
      */
@@ -64,11 +132,18 @@ var csThirdPartyCookie = function () {
         window.top.location.href = "//" + window.location.host + options.pathToCookieSetter +  "?" + parameters.join('&');
     }
 
-    // not in frame so third party cookie bug not an issue
+    /**
+     * Guard to check site is not loaded in a frame so cookei setting check not required
+     * 
+     * @returns {Boolean}
+     */
     function inFrame() {
         return (window.top !== window.self);
     }
 
+    /**
+     * Redirects the user back to the parent page and the framed page
+     */
     function returnToParent() {
         var returnUrl = getURLParameter(options.returnUriKey, location.search);
 
@@ -95,7 +170,12 @@ var csThirdPartyCookie = function () {
         window.location.href = returnUrl;
     }
 
-    function publicCheck() {
+    /**
+     * Checks the framed page can set cookies and redirects to the cookie setter if required
+     * 
+     * @returns {Boolean}
+     */
+    function check() {
         if (!inFrame()) {
             return true;
         }
@@ -117,13 +197,16 @@ var csThirdPartyCookie = function () {
         redirectToSetCookie();
     }
 
+    /**
+     * Sets a cookie and returns to parent and framed page
+     */
     function setAndReturn() {
         setCookie();
         returnToParent();
     }
 
     return {
-        check: publicCheck,
+        check: check,
         setAndReturn: setAndReturn
     };
 
